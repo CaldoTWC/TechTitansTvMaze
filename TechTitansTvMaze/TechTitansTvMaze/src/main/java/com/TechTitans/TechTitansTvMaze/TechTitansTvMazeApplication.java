47e9953c942c894ea3f272bdd6b03605a5ca0fd6
Aldo Salazar <136004329+CaldoTWC@users.noreply.github.com>
2024-12-01 21:50:18 -0600

    Add files via upload

---
+++ TechTitansTvMaze/TechTitansTvMaze/src/main/java/com/TechTitans/TechTitansTvMaze/TechTitansTvMazeApplication.java
@@ -0,0 +1,13 @@
+package com.TechTitans.TechTitansTvMaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTitansTvMazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTitansTvMazeApplication.class, args);
+	}
+
+}
---
+++ TechTitansTvMaze/TechTitansTvMaze/src/test/java/com/TechTitans/TechTitansTvMaze/TechTitansTvMazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechTitans.TechTitansTvMaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTitansTvMazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
